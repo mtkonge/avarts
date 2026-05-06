3592a5c949e63e803d9f8fc993e0cbaae8b57570
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2026-05-06 12:22:40 +0200

    json db impl

--- server/src/JsonDb.ts
+++ server/src/JsonDb.ts
@@ -1,24 +1,51 @@
 import type { Database } from "./Database.ts";
 import type { Route } from "./Route.ts";
-import * as fs from "@std/fs";
+import * as z from "zod";
+
+const coord = z.tuple([z.number(), z.number()]);
+
+const Route = z.strictObject({
+    id: z.number(),
+    coords: z.array(coord),
+});
+
+const Routes = z.array(Route);
 
 export class JsonDb implements Database {
-    private constructor() {
+    private static data_dir: string = "jsondb_data";
+    private constructor(private routes: Route[]) {
     }
 
     public static async open(): Promise<Database> {
-        await fs.ensureFile("jsondb_data/routes.json");
-        await Deno.writeTextFile("jsondb_data/.gitignore", "*");
-        return new JsonDb();
+        await Deno.writeTextFile(`${JsonDb.data_dir}/.gitignore`, "*", {
+            create: true,
+        });
+        const routes = await Deno.readTextFile(`${JsonDb.data_dir}/routes.json`)
+            .catch(() => "[]")
+            .then((x) => JSON.parse(x))
+            .then((x) => Routes.parse(x));
+        return new JsonDb(routes);
     }
 
     getRouteById(id: number): Route {
-        throw new Error("Method not implemented.");
+        const route = this.routes.find((x) => x.id === id);
+        if (!route) {
+            throw new Error(`invalid id ${id}`);
+        }
+        return structuredClone(route);
     }
     addRoute(route: Route): void {
-        throw new Error("Method not implemented.");
+        this.routes.push(route);
     }
     getAllRoutes(): Route[] {
-        throw new Error("Method not implemented.");
+        return structuredClone(this.routes);
+    }
+
+    async save() {
+        await Deno.writeTextFile(
+            `${JsonDb.data_dir}/routes.json`,
+            JSON.stringify(this.routes),
+            { create: true },
+        );
     }
 }
